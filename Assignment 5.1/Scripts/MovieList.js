10de4fe78e8ab94c2056a76d7713be5ecb2d53c7
Lucas Foxworthy <40660037+EverWand@users.noreply.github.com>
2024-06-15 03:04:21 -0700

    Finalized Style, Script works now

--- Assignment 5.1/Scripts/MovieList.js
+++ Assignment 5.1/Scripts/MovieList.js
@@ -1,22 +1,24 @@
 var MovieList = []; //Array used to store movies into
 
 //Functions needed to insert new movie submissions into the movie list
-function AddMovie(movieTitle){
+function addMovie(){
+    
     event.preventDefault(); //TEST OUT WHAT THIS DOES IF COMMENTED OUT! {THink it allows you to submit an event without refreshing the page?}
     var MovieTitleInput = document.getElementById("MovieName"); //set a variable to the input found in the Movie name submission
-    var MovieTitle = MovieTitleInput.ariaValueMax.trim(); //removes empty space from the input and set it as the Movie Name
+    var MovieTitle = MovieTitleInput.value.trim(); //removes empty space from the input and set it as the Movie Name
 
     //Has a Movie title been entered?
-    if(movieTitle){
-        MovieList.push(movieTitle); //Push the title to the list
+    if(MovieTitle){
+
+        MovieList.push(MovieTitle); //Push the title to the list
         MovieList.sort();           //Alphabetically sort the current list items
         MovieTitleInput.value = ""; //Clear the Movie submission text
         
-        UpdateMovieListDisplay();   //Update Movie List Display to include the new movie added
+        updateMovieListDisplay();   //Update Movie List Display to include the new movie added
     }
 }
 
-function UpdateMovieListDisplay(){
+function updateMovieListDisplay(){
     var FormattedMovieList = []; //empty array to hold html formatted movie list
     var divMovies = document.getElementById("MovieListDisplay");
 
@@ -24,6 +26,7 @@ function UpdateMovieListDisplay(){
     for(var i=0; i < MovieList.length; i++){
         FormattedMovieList.push("<div>" + MovieList[i] + "</div>"); //Format the movie item into html
     }    
+
     divMovies.innerHTML = FormattedMovieList.join(''); //turn the formatted list into a string
 }
 
@@ -31,5 +34,5 @@ function UpdateMovieListDisplay(){
 function clearMovieList(){
     MovieList = [];             //set the Movie list back to an empty array
 
-    UpdateMovieListDisplay();   //Update the Movie List Display
+    updateMovieListDisplay();   //Update the Movie List Display
 }

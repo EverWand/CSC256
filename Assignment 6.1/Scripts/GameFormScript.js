c96d04b6981838376a4e796de184997aa37c0ab5
Lucas Foxworthy <40660037+EverWand@users.noreply.github.com>
2024-06-22 23:02:04 -0700

    Added Comments

--- Assignment 6.1/Scripts/GameFormScript.js
+++ Assignment 6.1/Scripts/GameFormScript.js
@@ -1,17 +1,19 @@
+//Function to submit Player info from the player form
 function SubmitPlayerInfo(){
     event.preventDefault();
     
     //Obtain values entered in the specific text inputs
-    var name = document.getElementById("UsernameInput").value;      //Name
+    var name = document.getElementById("UsernameInput").value;  //Name
     var weapon = document.getElementById("WeaponInput").value;  //Weapon
     var health = document.getElementById("HealthInput").value;  //Health
-    var points = document.getElementById("PointsInput").value;   //Points
+    var points = document.getElementById("PointsInput").value;  //Points
 
     //Construct the output from the input
-    var output =    "Name: " + name;
-    output +=   "\n\nWeapon: " + weapon;
-    output +=   "\n\nHealth: " + health;
-    output +=   "\n\nPoints: " + points;
+    var output =    "Name: " + name;        //Set with Name
+    output +=   "\n\nWeapon: " + weapon;    //Append Weapon
+    output +=   "\n\nHealth: " + health;    //Append Health
+    output +=   "\n\nPoints: " + points;    //Append Points
 
-    document.getElementById("FormOutput").value = output;
+    //Display the output
+    document.getElementById("FormOutputDisplay").value = output;
 }

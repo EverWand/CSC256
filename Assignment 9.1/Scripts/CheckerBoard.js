d25d8656afd9d335bc10a5e5c946250bac80bfde
Lucas Foxworthy <40660037+EverWand@users.noreply.github.com>
2024-07-12 02:06:46 -0700

    I CAN FINALLY MOVE THE PIECES OMG OMG OMG
    This took way too long

--- Assignment 9.1/Scripts/CheckerBoard.js
+++ Assignment 9.1/Scripts/CheckerBoard.js
@@ -29,7 +29,7 @@ function MakeBoard(){
             checkerSpace.className =  "boardcell" //give the board cell the appropriate CSS
             checkerSpace.setAttribute("id", "Cell " + i + j); //give the cell an ID
 
-            //Color even cells white:
+            //Color non-even cells black:
             if((i+j) % 2 == 1)
             {
                 //set up cell and add it to the row                
@@ -38,7 +38,7 @@ function MakeBoard(){
                 checkerSpace.addEventListener("click", MovePiece);
                 
             }
-            //color odd cells black
+            //color even cells white
             else{
                 //set up cell and add it to the row
                 checkerSpace.classList.add('whitecell');
@@ -48,7 +48,7 @@ function MakeBoard(){
             tr.appendChild(checkerSpace);
 
             if(checkerPieces[i][j]){
-                CreatePiece("Piece " +i +j, "type-" + checkerPieces[i][j], checkerSpace)
+                CreatePiece("piece" +i +j, "CheckerPiece-" + checkerPieces[i][j], checkerSpace)
             }
         }
          //add the constucted row to the table
@@ -59,16 +59,59 @@ function MakeBoard(){
 
 //Creates A checker piece
 function CreatePiece(id, pieceClass, cell){
-    
+    //Make a container for the checker piece
+    var newPiece = document.createElement("div");
+    newPiece.setAttribute("id", id) // Set ID
+    //Add the required Checker Piece Classes
+    newPiece.classList.add("CheckerPiece"); //genera; checkerpiece css
+    newPiece.classList.add(pieceClass);     //dark or light piece
+
+    newPiece.addEventListener("click", savePieceID);    //Give it an event handler
+
+    cell.appendChild(newPiece); //Add piece to the board cell
 }
 
-//Used to update a slot if there's meant to be a piece in a cell
-function UpdatePieceDisplay(){
+function savePieceID(event)
+{
+    var pieceHoldSpan = document.getElementById("selectedCell");
 
+    var selectedPieceID = event.target.id; //hold the id of the piece
+    selectedPieceID = selectedPieceID.replace("div","").replace("piece", "");
+
+    //store the piece
+    pieceHoldSpan.textContent = selectedPieceID;
+    
+    console.log("selected Piece = " + selectedPieceID); //DEBUG: SHows the ID of selected Piece
 }
 
 //Logic for when a piece is moved
 function MovePiece(event)
 {
-    console.log("PIECE MOVE!");
+    var pieceHoldSpan = document.getElementById("selectedCell");
+    var newCell_ID = event.target.id; //Get the ID of the cell
+    
+    console.log(newCell_ID); //DEBUG: Check the Raw ID of the New Cell
+    
+    //remove ID name prefix
+    newCell_ID = newCell_ID.replace("Cell ","").replace("piece", "");
+
+    var selectedPieceID = pieceHoldSpan.textContent; //get piece held
+
+    if (selectedPieceID != newCell_ID)
+    {
+        //get the old cell
+        var oldCell = document.getElementById("Cell " + selectedPieceID);
+        //get the old piece
+        var oldPiece = document.getElementById("piece" + selectedPieceID);
+        //get the type of the old piece
+        var oldPieceType = oldPiece.classList[1];
+
+        //Remove the Old piece from the board
+        oldCell.removeChild(oldPiece);
+        
+        //Add the new piece position
+        var newCell = document.getElementById("Cell " + newCell_ID);
+        CreatePiece("piece"+ newCell, oldPieceType, newCell);
+        pieceHoldSpan.textContent = ""; //Clear the piece held by the span
+    }
 }

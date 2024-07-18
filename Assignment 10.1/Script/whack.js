4bc3c9777e9b911252a87888e5030b40c07839a5
Lucas Foxworthy <40660037+EverWand@users.noreply.github.com>
2024-07-18 05:43:37 -0700

    Finished Gameplay
    +Random holes are now chosen to be popped up
    +Holes are clickable
    + Added a Timer and Score/Points and they work

--- Assignment 10.1/Script/whack.js
+++ Assignment 10.1/Script/whack.js
@@ -1,6 +1,15 @@
+//VARIABLES:
+//---Holes
 var holeArr = [];
 var lasthole = 0;
+//---Points
+var currPoints = 0;
+const WACK_VALUE = 25; 
+//---Timer
+var gameTimerSeconds = 10;
+var timeInterval;
 
+//GAME FUNCTIONS
 function CreateHoles(amountOfHoles){
     var parent = document.getElementById("gameBoardContainer");
 
@@ -10,30 +19,68 @@ function CreateHoles(amountOfHoles){
         hole.setAttribute("id", "Hole " + i);
         hole.classList.add("hole");
 
-        parent.appendChild(hole);
+        hole.addEventListener("click", handle_WhackHole);
+        parent.appendChild(hole);   
     }
     
     holeArr = document.querySelectorAll(".hole");
 }
+function StartGame() {
+    
+    //set the time that the game needs to once the game starts
+    var endTime = Date.now() + gameTimerSeconds * 1000;
 
-function StartGame()
-{
-    timeUp = false
-    Pop_Up();
+        //Start a Timer interval function
+        timerInterval = setInterval(function()
+        {
+            UpdateTimerDisplay(endTime);
+    
+            if(Math.round((endTime - Date.now())/1000) <= 0){
+                clearInterval(timerInterval)
+            }
+    
+        }, 1000);
+
+    //Inline Function for recursively looping the game loop.
+    function gameLoop() {
+        //is the game still going?
+        if (Date.now() < endTime) {
+            //After a random amount of time:
+            setTimeout(function() {
+            
+                Reset_Holes();  // Reset all of the holes to no longer be popped up
+                PopUp_Hole();   // Get a random hole to pop up
+                gameLoop();     // === Recursive Loop ===
+            
+            }, RandomTime(500, 1000));
+        } 
+        //Is the game timer up?
+        else {
+            //Games' Ended
+            Reset_Holes();                  //Reset all of the holes
+            console.log("GAME ENDED!");     //Display Game Over
+        }
+    }
+    gameLoop(); // Start the game loop
+
 }
 
-function Pop_Up()
+//HOLE FUNCTIONS
+//---Reset the Hole Diplays
+function Reset_Holes(holes)
 {
-    var time = RandomTime(200, 1000); //random time between range
-   GetRandomHole(holeArr);
+    //For every hole inside of the holes array
+    holeArr.forEach(element => {
+        element.classList.remove("poppedUp");
+    });
 }
-
-function RandomTime(min, max)
+//---Pick a Whole to be popped up
+function PopUp_Hole()
 {
-    // 0%-100% of the cumulative value of Min and Max values wanted
-    return Math.round(Math.random() * (max-min) + min);
+    GetRandomHole(holeArr);
+    holeArr[lasthole].classList.add("poppedUp");
 }
-
+//---Returns a random Hole
 function GetRandomHole(holes)
 {
     //get random index from the hole list given
@@ -48,4 +95,47 @@ function GetRandomHole(holes)
 
     console.log("Hole shown: " + counter);
     return hole;
+}
+
+//UI FUNCTIONS
+//---Updates the Points Display
+function UpdateScoreDisplay()
+{
+    var PointUI = document.getElementById("Score_Points");
+
+    PointUI.textContent = currPoints;
+}
+//Updates teh display with the current seconds left in the game
+function UpdateTimerDisplay(TimerEnd){
+    timeDisplay = document.getElementById("Timer");  //Get the element for the display
+
+    timeRemaining = TimerEnd - Date.now();
+    timeDisplay.textContent = Math.round(timeRemaining/1000);             //Put the current time left into the display container
+}
+
+//GENERAL FUNCTIONS
+//---Gets a random time based on range
+function RandomTime(min, max)
+{
+    // 0%-100% of the cumulative value of Min and Max values wanted
+    return Math.round(Math.random() * (max-min) + min);
+}
+
+//EVENTS:
+//Whack a Hole
+function handle_WhackHole(event)
+{
+    let hole = event.target; //get the hole that was clicked on
+
+    //Does the hole have the class of it being poppedUp?
+    if (hole.classList.contains('poppedUp'))
+    {
+        //put the hole back down : remove the class
+        hole.classList.remove('poppedUp')
+
+        //SCORE POINTS!
+        currPoints += WACK_VALUE;   //increase the current points
+        UpdateScoreDisplay();       //Update the score display
+
+    }
 }
